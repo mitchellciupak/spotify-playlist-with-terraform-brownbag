921e465d5e62208d92c9c3a81f2a469e25d35c49
mitchellciupak <ciupak@purdue.edu>
2022-06-01 08:41:24 -0500

    initial work

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Spotify-Playlist-with-Terraform-IaC
+## Brown Bag
+## Time
+* 50 Min Total
+* Make Most of the Time Workshop Based
+## Audience
+* DTS
+## Scope == What is DevOps? Introduction & Workshop
+* Pipelines
+* GitHub
+* IaC
+
+## Description
+Have em commit to spotify terraform iac, merge, see pipeline, and see the playlist
---
+++ presentation.py
@@ -0,0 +1,34 @@
+#!/bin/python3.8
+
+##################################
+# DevOps Presentation Brown Bag
+# 20210921
+# Mitchell Ciupak
+##################################
+
+def Introduce_DevOps():
+    return
+def Introduce_Piplines():
+    return
+def Introduce_GitVersionControl():
+    return
+def Introduce_GitHub():
+    return
+
+def Introduction():
+    Introduce_DevOps()
+    Introduce_Piplines()
+    Introduce_GitVersionControl()
+    Introduce_GitHub()
+
+def Workshop():
+    # SignIntoGitHub()
+    # CloneTheRepository()
+    return
+
+def Agenda():
+    Introduction()
+    Workshop()
+
+if __name__ == "__main__":
+    Agenda()

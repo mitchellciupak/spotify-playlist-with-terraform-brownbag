12bea9235dcdb6a984a83dae0fb1b5aee1b45118
mitchellciupak <mjcre23@gmail.com>
2022-06-03 12:03:41 -0400

    pres agenda in py

--- presentation/presentation.py
+++ presentation/presentation.py
@@ -5,6 +5,39 @@
 # 20210921
 # Mitchell Ciupak
 ##################################
+# Agenda
+    # I. What is Infrastructure as Code?
+    # a. The Problem w/ Manual Configuration
+    # b. Infrastructure as Code (IaC)
+    # II. Popular Infrastructure as Code Tools
+    # a. Declarative v. Imperative
+    # b. Declarative Examples (Azure Blueprints, AWS Cloudformation, Cloud Deployment Manager, **Terraform**)
+    # c. Imperative Examples (AWS CKD)
+    # d. More abt Terraform
+    # III. Infrastructure Lifecycle
+    # a. What is an Infrastructure's Lifecycle?
+    # b. An Infrastructure Lifecycle Example
+    # IV. Provisioning v. Deployment v. Ochestration
+    # a. Provisioning
+    # b. Deployment
+    # c. Ochestration
+    # V. What is GitOps?
+    # VI. Terraform Re-Intro
+    # a. What is Terraform Again?
+    # b. Terraform Lifecycle
+    # c. Change Automation
+    # d. Execution Plans
+    # e. Resource Graphs
+    # f. Use Cases
+    # VII. (Workshop) Let's use some Terraform
+    # a. Code Review & How to Read HCL
+    # b. Get Connected to GitHub
+    # c. Find Your Song
+    # d. Create a PR
+    # e. Merge and Deploy!!!
+##################################
+
+
 
 def Introduce_DevOps():
     print("where the info is") 
@@ -33,3 +66,5 @@ def Agenda():
 
 if __name__ == "__main__":
     Agenda()
+
+

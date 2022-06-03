d3aeefd47a1fdec0dc42f504b6b0eceb07824d0d
mitchellciupak <mjcre23@gmail.com>
2022-06-03 12:57:35 -0400

    setup examples in main.tf and added destroy to workspace tasks

--- main.tf
+++ main.tf
@@ -11,39 +11,37 @@ provider "spotify" {
   api_key = var.spotify_api_key
 }
 
-data "spotify_track" "overkill" {
-  url = "https://open.spotify.com/track/4XdaaDFE881SlIaz31pTAG"
+#### Examples ####
 
-  ## Computed
-  # name = "Overkill"
-  # artists = ["0qPGd8tOMHlFZt8EA1uLFY"]
-  # album = "64ey3KHg3uepidKmJrb4ka"
+# Song by URL
+data "spotify_track" "ninetofive_dollyp" {
+  url = "https://open.spotify.com/track/4w3tQBXhn5345eUXDGBWZG?si=dc7cdcab78ef406d"
 }
 
-data "spotify_track" "blackwater" {
-  spotify_id = "4lE6N1E0L8CssgKEUCgdbA"
-
-  ## Computed
-  # name = "Blackwater"
-  # artists = ["0qPGd8tOMHlFZt8EA1uLFY"]
-  # album = "1AUS845POFhV3oDytPImEZ"
+# Song by ID
+data "spotify_track" "cheeseburger_jimmybuffet" {
+  # Take a look at the url below --> track == 6VeZ970uI0Yi6sjBgyFBrp
+  # https://open.spotify.com/track/6VeZ970uI0Yi6sjBgyFBrp?si=9089cdb316184b68
+  spotify_id = "6VeZ970uI0Yi6sjBgyFBrp"
 }
 
-data "spotify_search_track" "by_artist" {
-  artist = "Dolly Parton"
-  #  album = "Jolene"
-  #  name  = "Early Morning Breeze"
+## List of Songs by Artist Search
+data "spotify_search_track" "queen_topsongs" {
+  artist = "Queen"
+  #  album = "A Night At The Opera"
+  #  name  = "Bohemian Rhapsody - Remastered 2011"
 }
 
 resource "spotify_playlist" "playlist" {
-  name        = "Terraform Playlist"
-  description = "This playlist was created by Terraform"
+  name        = "Nashville 2022 Brownbag Playlist"
+  description = "Learn more at https://github.com/mitchellciupak/spotify-playlist-with-terraform-brownbag"
   public      = true
 
   tracks = [
-    data.spotify_search_track.by_artist.tracks[0].id,
-    data.spotify_search_track.by_artist.tracks[2].id,
-    data.spotify_track.blackwater.id,
-    data.spotify_track.overkill.id,
+    data.spotify_track.ninetofive_dollyp.id,
+    data.spotify_track.cheeseburger_jimmybuffet.id,
+    data.spotify_search_track.queen_topsongs.tracks[0].id,
+    data.spotify_search_track.queen_topsongs.tracks[1].id,
+    data.spotify_search_track.queen_topsongs.tracks[2].id,    
   ]
 }

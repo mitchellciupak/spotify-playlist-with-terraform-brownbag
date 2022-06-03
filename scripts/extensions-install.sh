2930b19b0e87529b87f90232c50e74b515bb4421
mitchellciupak <mjcre23@gmail.com>
2022-06-03 12:13:07 -0400

    added extensions installation script and updated vs code workspace

---
+++ scripts/extensions-install.sh
@@ -0,0 +1,4 @@
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension HashiCorp.terraform
+code --install-extension ms-python.python

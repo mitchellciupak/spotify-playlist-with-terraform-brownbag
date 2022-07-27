93aed9ddf69e2a06a78a9f3d2bbacf276fedeff0
hnteeter <110119308+hnteeter@users.noreply.github.com>
2022-07-27 12:46:26 -0500

    Since u been goneeeeeee - Halle

--- main.tf
+++ main.tf
@@ -32,20 +32,23 @@ data "spotify_search_track" "queen_topsongs" {
   #  name  = "Bohemian Rhapsody - Remastered 2011"
 }
 
-data "spotify_track" "cheeseburger_jimmybuffet" {
-  spotify_id = "6VeZ970uI0Yi6sjBgyFBrp"
-}
+#### Place Your Song Here ####
 
 ## Added by Sydney Cholewinski
 data "spotify_track" "callmeback_thestrokes" {
   spotify_id = "6z5mh42xSM54RncKZMM8hS"
 }
 
-## Added by Mithell Ciupak
-data "spotify_track" "dreams_fleetwoodmac" {
-  spotify_id = "0ofHAoxe9vBkTCp2UQIavz"
-}
+#### Place Your Song Here ####
 
+## Added by Mithell Ciupak
+data "spotify_track" "cheeseburger_jimmybuffet" {
+  spotify_id = "6VeZ970uI0Yi6sjBgyFBrp"
+}
+## Added by Halle Teeter
+data "spotify_track" "Sinceubeengone_KellyClarkson" {
+  spotify_id = "04B9ThftHDqviE4uaUJMf1"
+}
 
 #### Playlsit ####
 
@@ -59,8 +62,8 @@ resource "spotify_playlist" "playlist" {
     data.spotify_track.jolene_dollyp.id,
     data.spotify_search_track.queen_topsongs.tracks[0].id,
     data.spotify_search_track.queen_topsongs.tracks[1].id,
-    data.spotify_search_track.queen_topsongs.tracks[2].id,
-    data.spotify_track.dreams_fleetwoodmac.id
+    data.spotify_search_track.queen_topsongs.tracks[2].id,    
+    # ADD YOUR SONG TO THE PLAYLIST HERE #
     data.spotify_track.cheeseburger_jimmybuffet.id,
     data.spotify_track.callmeback_thestrokes.id
   ]
